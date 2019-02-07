30d8326d38b1d65d96c0395a16478b789c558ae3
Kazuki Iida <elkel53930@gmail.com>
2019-02-07 23:33:15 +0900

    generate model

--- ch04/generative_model.py
+++ ch04/generative_model.py
@@ -5,9 +5,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 K = 3
+N = 1000
 
-sk = np.arange(K)
-pk = (0.3,0.6,0.1)
+mean = [ np.array([0,0])
+       , np.array([10,3])
+       , np.array([5,9])]
 
-cat = rv_discrete(name='cat', values=(sk,pk))
-x = cat.rvs(size=100)
+cov = [ np.array([[1,0],[0,2]])
+      , np.array([[5,1.5],[1,2]])
+      , np.array([[1,2],[2,7]])]
+
+vk = np.arange(K)
+pk = (0.3,0.4,0.3)
+sk = np.zeros((N,K))
+
+cat = rv_discrete(name='cat', values=(vk,pk))
+xs = []
+ys = []
+for i in range(N):
+    sk = cat.rvs()
+    (x, y) = mult.rvs(mean=mean[sk],cov=cov[sk])
+    xs = xs + [x]
+    ys = ys + [y]
+
+plt.scatter(xs,ys)
+plt.show()
